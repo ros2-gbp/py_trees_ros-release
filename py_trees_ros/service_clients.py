6a50abf072c1fe0673b0193fa754493d03b21ec9
Sebastian Castro <sebas.a.castro@gmail.com>
2025-11-13 19:00:59 -0500

    Rebase from 'debian/kilted/py_trees_ros'

--- py_trees_ros/service_clients.py
+++ py_trees_ros/service_clients.py
@@ -16,11 +16,12 @@ Behaviours for ROS services.
 # Imports
 ##############################################################################
 
-from asyncio.tasks import wait_for
 import typing
 import uuid
+from abc import ABC, abstractmethod
 
 import py_trees
+import rclpy.callback_groups
 
 from . import exceptions
 
@@ -31,7 +32,7 @@ from . import exceptions
 
 class FromBlackboard(py_trees.behaviour.Behaviour):
     """
-    An service client interface that draws requests from the blackboard. The
+    A service client interface that draws requests from the blackboard. The
     lifecycle of this behaviour works as follows:
 
     * :meth:`initialise`: check blackboard for a request and send
@@ -56,6 +57,7 @@ class FromBlackboard(py_trees.behaviour.Behaviour):
         key_request: name of the key for the request on the blackboard
         key_response: optional name of the key for the response on the blackboard (default: None)
         wait_for_server_timeout_sec: use negative values for a blocking but periodic check (default: -3.0)
+        callback_group: callback group for the service client
 
     .. note::
        The default setting for timeouts (a negative value) will suit
@@ -68,13 +70,16 @@ class FromBlackboard(py_trees.behaviour.Behaviour):
                  service_type: typing.Any,
                  service_name: str,
                  key_request: str,
-                 key_response: typing.Optional[str]=None,
-                 wait_for_server_timeout_sec: float=-3.0
+                 key_response: str | None = None,
+                 wait_for_server_timeout_sec: float = -3.0,
+                 callback_group: typing.Optional[rclpy.callback_groups.CallbackGroup] = None,
                  ):
         super().__init__(name)
         self.service_type = service_type
         self.service_name = service_name
         self.wait_for_server_timeout_sec = wait_for_server_timeout_sec
+        self.callback_group = callback_group
+
         self.blackboard = self.attach_blackboard_client(name=self.name)
         self.blackboard.register_key(
             key="request",
@@ -108,12 +113,16 @@ class FromBlackboard(py_trees.behaviour.Behaviour):
         """
         self.logger.debug("{}.setup()".format(self.qualified_name))
         try:
-            self.node = kwargs['node']
+            self.node = kwargs["node"]
         except KeyError as e:
             error_message = "didn't find 'node' in setup's kwargs [{}][{}]".format(self.qualified_name)
             raise KeyError(error_message) from e  # 'direct cause' traceability
 
-        self.service_client = self.node.create_client(srv_type=self.service_type, srv_name=self.service_name)
+        self.service_client = self.node.create_client(
+            srv_type=self.service_type,
+            srv_name=self.service_name,
+            callback_group=self.callback_group,
+        )
 
         result = None
         if self.wait_for_server_timeout_sec > 0.0:
@@ -122,7 +131,7 @@ class FromBlackboard(py_trees.behaviour.Behaviour):
             result = True # don't wait and don't check if the server is ready
         else:
             iterations = 0
-            period_sec = -1.0*self.wait_for_server_timeout_sec
+            period_sec = -1.0 * self.wait_for_server_timeout_sec
             while not result:
                 iterations += 1
                 result = self.service_client.wait_for_service(timeout_sec=period_sec)
@@ -148,7 +157,7 @@ class FromBlackboard(py_trees.behaviour.Behaviour):
 
     def initialise(self):
         """
-        Reset the internal variables and kick off a new request request.
+        Reset the internal variables and kick off a new request.
         """
         self.logger.debug("{}.initialise()".format(self.qualified_name))
 
@@ -158,8 +167,13 @@ class FromBlackboard(py_trees.behaviour.Behaviour):
         try:
             if self.service_client.service_is_ready():
                 self.service_future = self.service_client.call_async(self.blackboard.request)
-        except (KeyError, TypeError):
-            pass  # self.service_future will be None, check on that
+        except TypeError:
+            expected_type = self.service_type.Request.__name__
+            received_type = type(self.blackboard.request).__name__
+            self.logger.error(f"Received a request of type <{received_type}> instead of <{expected_type}>")
+        except KeyError as e:
+            self.logger.error(f"{e}")
+        # self.service_future will be None on either exception, and update will return FAILURE
 
     def update(self):
         """
@@ -185,7 +199,7 @@ class FromBlackboard(py_trees.behaviour.Behaviour):
             if self.write_response_to_blackboard:
                 self.blackboard.response = self.response
             return py_trees.common.Status.SUCCESS
-        
+
     def terminate(self, new_status: py_trees.common.Status):
         """
         If running and the current request has not already succeeded, cancel it.
@@ -224,6 +238,7 @@ class FromConstant(FromBlackboard):
         service_request: the request to send
         key_response: optional name of the key for the response on the blackboard (default: None)
         wait_for_server_timeout_sec: use negative values for a blocking but periodic check (default: -3.0)
+        callback_group: callback group for the service client
 
     .. note::
        The default setting for timeouts (a negative value) will suit
@@ -236,18 +251,20 @@ class FromConstant(FromBlackboard):
                  service_type: typing.Any,
                  service_name: str,
                  service_request: typing.Any,
-                 key_response: typing.Optional[str]=None,
-                 wait_for_server_timeout_sec: float=-3.0
+                 key_response: str | None = None,
+                 wait_for_server_timeout_sec: float = -3.0,
+                 callback_group: typing.Optional[rclpy.callback_groups.CallbackGroup] = None,
                  ):
         unique_id = uuid.uuid4()
-        key_request = "/goal_" + str(unique_id)
+        key_request = "/request_" + str(unique_id)
         super().__init__(
             service_type=service_type,
             service_name=service_name,
             key_request=key_request,
             key_response=key_response,
             name=name,
-            wait_for_server_timeout_sec=wait_for_server_timeout_sec
+            wait_for_server_timeout_sec=wait_for_server_timeout_sec,
+            callback_group=callback_group,
         )
         # parent already instantiated a blackboard client
         self.blackboard.register_key(
@@ -255,3 +272,120 @@ class FromConstant(FromBlackboard):
             access=py_trees.common.Access.WRITE,
         )
         self.blackboard.set(name=key_request, value=service_request)
+
+
+class AttributesFromBlackboard(FromBlackboard):
+    """
+    Convenience version of the service client that creates a request with fields read from BB.
+
+    Args:
+        name (str): Name of the behaviour
+        service_type (typing.Any): Type of the service
+        service_name (str): Endpoint of the service
+        request_fields (dict[str, typing.Any]): Fields of the request mapped to blackboard variables
+        wait_for_server_timeout_sec (float, optional): Wait timeout for the service. Defaults to -3.0.
+        callback_group: callback group for the service client
+    """
+
+    def __init__(self,
+                 name: str,
+                 service_type: typing.Any,
+                 service_name: str,
+                 request_fields: dict[str, typing.Any],
+                 wait_for_server_timeout_sec: float = -3.0,
+                 callback_group: typing.Optional[rclpy.callback_groups.CallbackGroup] = None,
+                 ):
+        unique_id = uuid.uuid4()
+        self.key_request = "/request_" + str(unique_id)
+        super().__init__(
+            service_type=service_type,
+            service_name=service_name,
+            key_request=self.key_request,
+            name=name,
+            wait_for_server_timeout_sec=wait_for_server_timeout_sec,
+            callback_group=callback_group,
+        )
+        # The parent constructor already instantiated a blackboard client
+        self.request_fields = request_fields
+        for bb_key in self.request_fields.values():
+            self.blackboard.register_key(
+                key=bb_key,
+                access=py_trees.common.Access.READ,
+            )
+        self.blackboard.register_key(
+            key=self.key_request,
+            access=py_trees.common.Access.WRITE,
+        )
+
+    def initialise(self):
+        """
+        Read from the blackboard the attributes to create a Request object; if succeeded, write it to the blackboard.
+        """
+        self.logger.debug("%s.initialise()" % self.__class__.__name__)
+        request_attributes = {request_attr: self.blackboard.get(bb_key) for request_attr, bb_key in
+                              self.request_fields.items()}
+        request = self.service_type.Request(**request_attributes)
+        self.blackboard.set(name=self.key_request, value=request)
+
+        super().initialise()
+
+
+class FromCallback(FromBlackboard, ABC):
+    """
+    Convenience version of the service client that obtains the request from a callback implemented
+    by derived classes.
+
+    .. see-also: :class:`py_trees_ros.service_clients.FromBlackboard`
+
+    Args:
+        name: name of the behaviour
+        name: name of the behaviour
+        service_type: spec type for the service
+        service_name: where you can find the service
+        key_response: optional name of the key for the response on the blackboard (default: None)
+        wait_for_server_timeout_sec: use negative values for a blocking but periodic check (default: -3.0)
+        callback_group: callback group for the service client
+
+    .. note::
+       The default setting for timeouts (a negative value) will suit
+       most use cases. With this setting the behaviour will periodically check and
+       issue a warning if the server can't be found. Actually aborting the setup can
+       usually be left up to the behaviour tree manager.
+    """
+    def __init__(self,
+                 name: str,
+                 service_type: typing.Any,
+                 service_name: str,
+                 key_response: str | None = None,
+                 wait_for_server_timeout_sec: float = -3.0,
+                 callback_group: typing.Optional[rclpy.callback_groups.CallbackGroup] = None,
+                 ):
+        unique_id = uuid.uuid4()
+        self.key_request = "/request_" + str(unique_id)
+        super().__init__(
+            service_type=service_type,
+            service_name=service_name,
+            key_request=self.key_request,
+            key_response=key_response,
+            name=name,
+            wait_for_server_timeout_sec=wait_for_server_timeout_sec,
+            callback_group=callback_group,
+        )
+        # parent already instantiated a blackboard client
+        self.blackboard.register_key(
+            key=self.key_request,
+            access=py_trees.common.Access.WRITE,
+        )
+
+    def initialise(self):
+        """
+        Call derived class `get_request` method and write the returned service request to the blackboard.
+        """
+        self.logger.debug("%s.initialise()" % self.__class__.__name__)
+        self.blackboard.set(name=self.key_request, value=self.get_request())
+
+        super().initialise()
+
+    @abstractmethod
+    def get_request(self):
+        pass
